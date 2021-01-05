0b582662944e085b0fadf52053aa168a76012faa
Hursh Gupta <hurshgupta1@gmail.com>
2021-01-05 16:10:57 +0530

    updated block identities

--- SpriteImages.py
+++ SpriteImages.py
@@ -2,11 +2,11 @@ import pygame
 
 level_1 = [0 for i in range(6)]
 level_1[0] = pygame.Surface((40,40))
-level_1[1] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/block_square.png')
-level_1[2] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/Dirt-AM.bmp')
-level_1[3] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/Grass-L.bmp')
-level_1[4] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/Grass-M.bmp')
-level_1[5] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/Grass-R.bmp')
+level_1[1] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/full block.png')
+level_1[2] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/top left.png')
+level_1[3] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/top right.png')
+level_1[4] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/bottom right.png')
+level_1[5] = pygame.image.load(r'./WorldData/Level 1/Sprites/World/bottom left.png')
 
 for i in range(len(level_1)):
     level_1[i] = pygame.transform.scale(level_1[i],(40,40))
